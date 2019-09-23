72b08f7bf10286e975f00b6dfbbc5da15b66ae25
Sukhpreet Singh Anand <sukhpreet.singhanand@gmail.com>
2019-09-22 21:27:23 -0700

    User tasks 24,25,27: Added navigation bar for switching between signup and signin pages. Added fields for username and password.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserService } from './shared/user.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/signin/signin.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './user/signup/signup.component';
+import { appRoutes } from './routes';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthInterceptor } from './auth/auth.interceptor';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    UserComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [UserService,AuthGuard,
+    ,
+    {
+      provide : HTTP_INTERCEPTORS,
+      useClass : AuthInterceptor,
+      multi : true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router'
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/signup/signup.component';
+import { SignInComponent } from './user/signin/signin.component';
+import { AuthGuard } from './auth/auth.guard';
+
+export const appRoutes: Routes = [
+    { path: 'home', component: HomeComponent,canActivate:[AuthGuard] },
+    {
+        path: 'signup', component: UserComponent,
+        children: [{ path: '', component: SignUpComponent }]
+    },
+    {
+        path: 'login', component: UserComponent,
+        children: [{ path: '', component: SignInComponent }]
+    },
+    { path : '', redirectTo:'/login', pathMatch : 'full'}
+    
+];
