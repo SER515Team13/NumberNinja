878e65a88e834c8b8cbd066aaa0c82c34b890ea9
Sagar-k93 <sagark93@gmail.com>
2019-09-23 04:45:03 -0700

    Admin UI part to view new profiles and assign roles complete.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  role: string;
+  action: string;
+}
+
+export interface Role {
+  name: string;
+  value: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', role: 'Student', action: 'A'},
+  {position: 2, name: 'Helium', role: '', action: 'A'},
+  {position: 3, name: 'Lithium', role: 'Student', action: 'A'},
+  {position: 4, name: 'Beryllium', role: 'Student', action: 'A'},
+  {position: 5, name: 'Boron', role: 'Student', action: 'A'},
+  {position: 6, name: 'Carbon', role: 'Teacher', action: 'A'},
+  {position: 7, name: 'Nitrogen', role: 'Student', action: 'A'},
+  {position: 8, name: 'Oxygen', role: '', action: 'A'},
+  {position: 9, name: 'Fluorine', role: 'Teacher', action: 'D'},
+  {position: 10, name: 'Neon', role: 'Student', action: 'A'},
+];
+
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
+
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['position', 'name', 'role', 'action'];
+  dataSource = ELEMENT_DATA;
+  roles  = ['Student' , 'Teacher'];
+
+  roleControl = new FormControl('', [ Validators.required]);
+
+  constructor() { 
+    this.dataSource = ELEMENT_DATA;
+
+  }
 
   ngOnInit() {
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatTableModule } from '@angular/material' ;
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select'; 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,12 @@ import { AdminComponent } from './admin/admin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
