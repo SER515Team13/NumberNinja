98f49d42839dd98650e21830d51e5146153b8984
Sagar-k93 <sagark93@gmail.com>
2019-11-03 23:07:23 -0700

    clear build errors

---
+++ NumberNinjaClient/src/app/admin/existuser/existuser.component.ts
@@ -0,0 +1,145 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material';
+import { HttpService } from "../../shared/http.services";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserService } from '../../shared/user.service';
+import { User } from '../../shared/user.model';
+
+const userRoles: String[] = ['student',
+  'teacher','admin'
+];
+
+const grades: String[] = ['2',
+'7'
+];
+@Component({
+  selector: 'app-existuser',
+  templateUrl: './existuser.component.html',
+  styleUrls: ['./existuser.component.css']
+})
+export class ExistuserComponent implements OnInit {
+  private taskMessage: string = "EXISTING ACCOUNTS";
+  private noUsersMessage: string = "NO APPROVED USERS";
+
+  private roles: String[] = userRoles;
+  private classes: String[] = grades;
+  public allData: User[];
+  private isDisabled = true;
+  private isEdit = false;
+  private userRole = null;
+  private userGrade = null;
+  private responseFromAPi;
+  private dataSource = null;
+  private displayedColumns: String[] = ['firstName', 'lastName', 'email', 'role', 'grade', 'action'];
+  private roleControl = new FormControl('', [Validators.required]);
+
+  constructor(public http: HttpClient, private userService:UserService) {
+  }
+
+  ngOnInit() {
+    var userData = this.userService.getAllExistingUsers().subscribe((data: any) => {
+      if (data && data != undefined && data.length) {
+        console.log("Inside");
+        this.dataSource = new MatTableDataSource<User>(data);
+        console.log(data);
+      }
+    });
+    this.responseFromAPi = userData;
+  }
+  readonly rootUrl = 'http://localhost:3000';
+
+  
+
+  public acceptUser(selectedUser: User) {
+    const data = this.dataSource.data;
+    const index: number = data.indexOf(selectedUser);
+    var callServertoAddRole = selectedUser;
+    callServertoAddRole['flag'] = true;
+    this.userService.addDelete(callServertoAddRole).subscribe((data:any) => {
+      if(data.key !== '') {
+        console.log(data);
+      } else {
+        console.log("failed");
+      }
+    });
+    if (index !== -1) {
+      data.splice(index, 1);
+      this.dataSource.data = data;
+      this.triggerEmail(selectedUser, true).subscribe(
+        data => {
+          let res: any = data;
+          console.log("Mail has been sent to the user.");
+        });
+
+    }
+    console.log(selectedUser);
+  }
+
+  public isAcceptDisabled(element : any) {
+      if(element.role == 'teacher' || element.role == 'student') {
+        if(element.grade == null ) {
+          element.isDisabled = true;
+        }
+        else {
+          element.isDisabled = false;
+        }
+      }
+      else if(element.role == 'admin') {
+        element.isDisabled = false;
+      }
+      else {
+        element.isDisabled =  true;
+    }
+  }
+
+  public refresh(element : any) {
+    element.userGrade = element.grade;
+  }
+
+  public delete(selectedUser: User) {
+    const data = this.dataSource.data;
+    const index: number = data.indexOf(selectedUser);
+    var callServertoAddRole = selectedUser;
+    callServertoAddRole['flag'] = false;
+    this.userService.addDelete(callServertoAddRole).subscribe((data:any) => {
+      if(data.key !== '') {
+        console.log(data);
+      } else {
+        console.log("failed");
+      }
+    });
+    if (index !== -1) {
+      data.splice(index, 1);
+      if (data && data != undefined && data.length) {
+        this.dataSource.data = data;
+      } else {
+        this.dataSource = null;
+      }
+  }
+}
+
+  public editUser(selectedUser: User) {
+    var callServertoAddRole = selectedUser;
+    callServertoAddRole['flag'] = true;
+    this.userService.addDelete(callServertoAddRole).subscribe((data:any) => {
+      if(data.key !== '') {
+        console.log(data);
+      } else {
+        console.log("failed"); 
+      }
+    });
+  }
+
+  triggerEmail(currentUser: User, requestAccepted: boolean): Observable<{}> {
+    console.log("Sending email to the User.");
+    let body = {
+      name: currentUser.firstName,
+      email: currentUser.email,
+      requestAccepted: requestAccepted
+    }
+    const reqHeader = new HttpHeaders({ 'No-Auth': 'True' });
+    return this.http.post(this.rootUrl + '/sendmail', body, { headers: reqHeader });
+  }
+}
---
+++ NumberNinjaClient/src/app/admin/pending/admin.component.spec.ts
@@ -0,0 +1,69 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AdminComponent } from './admin.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminToolbarComponent } from '../../toolbars/admintoolbar/admintoolbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from '../../shared/user.service';
+
+describe('AdminComponent', () => {
+  let component: AdminComponent;
+  let fixture: ComponentFixture<AdminComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        MatTableModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        RouterTestingModule
+      ],
+      declarations: [
+        AdminComponent,
+        AdminToolbarComponent
+      ],
+      providers: [
+        UserService
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  xit('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  xit('should have a table', () => {
+    const compiledDom = fixture.debugElement.nativeElement;
+    compiledDom.querySelector('mat-table').click();
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('.matTable')).length).toEqual(1);
+  });
+
+  xit('should have defined number of Columns', () => {
+    const compiledDom = fixture.debugElement.nativeElement;
+    compiledDom.querySelector('mat-header-cell').click();
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('.mat-header-cell')).length).toEqual(5);
+  });
+
+  xit('should have accept and reject buttons', () => {
+    fixture.detectChanges();
+    for (let entry of fixture.debugElement.queryAll(By.css('.mat-cell.mat-column-action'))) {
+      expect(entry.queryAll(By.css('.btn')).length).toEqual(2);
+    }
+  });
+});
