ebe9bc107fdbfca96d6f5be62d2fb6ee49c80daa
Sukhpreet Singh Anand <sukhpreet.singhanand@gmail.com>
2019-11-07 21:34:40 -0700

    Initial canvas page trial

--- NumberNinjaClient/src/app/student/canvas/solve-question/solve-question.component.ts
+++ NumberNinjaClient/src/app/student/canvas/solve-question/solve-question.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Operator } from './../models/operator.model';
+import { OperatorService } from './../services/operator.service';
 
 @Component({
   selector: 'app-solve-question',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solve-question.component.css']
 })
 export class SolveQuestionComponent implements OnInit {
+  operators: Operator[] = [];
+  slots: Operator[] = [];
 
-  constructor() { }
+  constructor(private operatorservice: OperatorService) { }
+
+  drop (event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(this.operators, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
 
   ngOnInit() {
+    const operatorsObservable = this.operatorservice.getOperators();
+    operatorsObservable.subscribe((operatorsData: Operator[]) => {
+      this.operators = operatorsData;
+    });
   }
 
 }
